c4d4d09fbe1769e21423a764107c1ea653dc6f61
Suleiman <hersi.sa@gmail.com>
2023-02-27 13:00:51 +0100

    update: solidity versions and hardhat tasks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,20 +1,55 @@
-// import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+
+
+import { task } from "hardhat/config";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { BigNumber } from "ethers";
+// import "@nomiclabs/hardhat-waffle";
 import "@openzeppelin/hardhat-upgrades";
 import * as dotenv from "dotenv";
-// require("@nomiclabs/hardhat-waffle");
+
+
 dotenv.config();
 
 const FUJI_URL = process.env.FUJI_URL
+const CCHAIN_URL = process.env.CCHAIN_URL
+
 
 
 // Replace these private key with your Avalanche account private keys
 // Be aware of NEVER adding private keys to GIT
 const AVALANCHE_TEST_PRIVATE_KEY = process.env.AVALANCHE_TEST_PRIVATE_KEY;
-const AVALANCHE_MAIN_PRIVATE_KEY = process.env.AVALANCHE_TEST_PRIVATE_KEY;
+const AVALANCHE_MAIN_PRIVATE_KEY = process.env.AVALANCHE_MAIN_PRIVATE_KEY;
+
+task("accounts", "Prints the list of accounts", async (args, hre): Promise<void> => {
+  const accounts: SignerWithAddress[] = await hre.ethers.getSigners()
+  accounts.forEach((account: SignerWithAddress): void => {
+    console.log(account.address)
+  })
+})
+
+task("balances", "Prints the list of AVAX account balances", async (args, hre): Promise<void> => {
+  const accounts: SignerWithAddress[] = await hre.ethers.getSigners()
+  for(const account of accounts){
+    const balance: BigNumber = await hre.ethers.provider.getBalance(
+      account.address
+    );
+    console.log(`${account.address} has balance ${balance.toString()}`);
+  }
+})
+
+
 
 export default {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0"
+      },
+      {
+        version: "0.8.9"
+      }
+    ]
+  },
   networks: {
     avalancheTest: {
       url: FUJI_URL,
@@ -23,7 +58,7 @@ export default {
       accounts: [`0x${AVALANCHE_TEST_PRIVATE_KEY}`]
     },
     avalancheMain: {
-      url: 'https://api.avax.network/ext/bc/C/rpc',
+      url: CCHAIN_URL,
       gasPrice: 225000000000,
       chainId: 43114,
       accounts: [`0x${AVALANCHE_MAIN_PRIVATE_KEY}`]
