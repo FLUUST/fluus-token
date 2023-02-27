aff66580fd60b2126140ce242ed4215eeddce1af
Suleiman <hersi.sa@gmail.com>
2023-02-27 16:40:20 +0100

    deployed to testnet at 0x319D228C9f16e8c45F892ca86D192a703DB51258

--- README.md
+++ README.md
@@ -20,3 +20,14 @@ You can target any network from your Hardhat config using:
 ```
 npx hardhat run --network <network-name> scripts/deploy.ts
 ```
+
+
+## FLUUS Token Allocation 
+int256
+Charitable 50000000000000000000000000
+Marketing 100000000000000000000000000
+Community 200000000000000000000000000
+Operating 140000000000000000000000000
+Team 150000000000000000000000000
+Liquidity 90000000000000000000000000
+Participants 220000000000000000000000000
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,7 @@ import { BigNumber } from "ethers";
 // import "@nomiclabs/hardhat-waffle";
 import "@openzeppelin/hardhat-upgrades";
 import * as dotenv from "dotenv";
+import { EtherscanProvider } from "@ethersproject/providers";
 
 
 dotenv.config();
@@ -19,32 +20,44 @@ const CCHAIN_URL = process.env.CCHAIN_URL
 // Be aware of NEVER adding private keys to GIT
 const AVALANCHE_TEST_PRIVATE_KEY = process.env.AVALANCHE_TEST_PRIVATE_KEY;
 const AVALANCHE_MAIN_PRIVATE_KEY = process.env.AVALANCHE_MAIN_PRIVATE_KEY;
+const API_KEY = process.env.API_KEY;
 
-task("accounts", "Prints the list of accounts", async (args, hre): Promise<void> => {
-  const accounts: SignerWithAddress[] = await hre.ethers.getSigners()
-  accounts.forEach((account: SignerWithAddress): void => {
-    console.log(account.address)
-  })
-})
-
-task("balances", "Prints the list of AVAX account balances", async (args, hre): Promise<void> => {
-  const accounts: SignerWithAddress[] = await hre.ethers.getSigners()
-  for(const account of accounts){
-    const balance: BigNumber = await hre.ethers.provider.getBalance(
-      account.address
-    );
-    console.log(`${account.address} has balance ${balance.toString()}`);
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
   }
-})
+});
 
+task(
+  "balances",
+  "Prints the list of AVAX account balances",
+  async (args, hre): Promise<void> => {
+    const accounts: SignerWithAddress[] = await hre.ethers.getSigners();
+    for (const account of accounts) {
+      const balance: BigNumber = await hre.ethers.provider.getBalance(
+        account.address
+      );
+      console.log(`${account.address} has balance ${balance.toString()}`);
+    }
+  }
+);
 
 
 export default {
+  etherscan: {
+    // Your API key for Snowtrace
+    // Obtain one at https://snowtrace.io/
+    apiKey: API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
   solidity: {
     compilers: [
-      {
-        version: "0.8.0"
-      },
       {
         version: "0.8.9"
       }
